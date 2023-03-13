9e336a5a1f2834bcc90e97f53aff788e4daf2598
Markus Korn <markus.korn@gmail.com>
2023-03-13 11:49:56 +0100

    darwin homebrew: add a few mas apps

--- hosts/darwin/shared/homebrew.nix
+++ hosts/darwin/shared/homebrew.nix
@@ -4,7 +4,17 @@
     caskArgs.no_quarantine = true;
     global.brewfile = true;
     masApps = {
-      # Xcode = 497799835;
+      Xcode = 497799835;
+      UTM = 1538878817;
+      TestFlight = 899247664;
+      #"Microsoft Remote Desktop" = 1295203466;
+      "Microsoft Word" = 462054704;
+      "Microsoft Excel" = 462058435;
+      "Microsoft Powerpoint" = 462062816;
+      #"Microsoft Outlook" = 985367838;
+      "Microsoft OneDrive" = 823766827;
+      #"Microsoft ToDo" = 1274495053;
+      # Slack = 803453959;
     };
     casks = [
       "raycast"
