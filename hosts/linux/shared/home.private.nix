f438ff193ab8382129a0f4fce923c02f690e7bdf
Markus Korn <markus.korn@gmail.com>
2023-08-26 09:17:06 +0200

    feat(home.private.nix): set default user shell to zsh for better shell experience

--- hosts/linux/shared/home.private.nix
+++ hosts/linux/shared/home.private.nix
@@ -5,6 +5,7 @@
         home = "/home/thekorn";
         isNormalUser = true;
         extraGroups = [ "wheel" ];
+        defaultUserShell = pkgs.zsh;
         packages = with pkgs; [ git neovim htop tree ];
       };
     };
