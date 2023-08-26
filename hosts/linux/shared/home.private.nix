245752256a21c572dbd70a177da48fdf4d55c16e
Markus Korn <markus.korn@gmail.com>
2023-08-26 09:12:09 +0200

    fix(home.private.nix): change thekorn user home directory path from "/Users/thekorn" to "/home/thekorn" to match Linux directory structure

--- hosts/linux/shared/home.private.nix
+++ hosts/linux/shared/home.private.nix
@@ -2,7 +2,7 @@
   users = {
     users = {
       thekorn = {
-        home = "/Users/thekorn";
+        home = "/home/thekorn";
         isNormalUser = true;
         extraGroups = [ "wheel" ];
         packages = with pkgs; [ git neovim htop tree ];
