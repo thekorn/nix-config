fd4295bc610e0f204a56e91491ad97aa2ff7c48d
Markus Korn <markus.korn@gmail.com>
2024-06-01 12:38:33 +0200

    Refactor gptcommit setup and configuration
    - Streamline commit message preparation script and update path configurations
    - Introduce new TOML configuration file for `gptcommit` with extensive settings including API, prompt customization, and file exclusions
    - Adjust project structure and settings across multiple platform configurations (VS Code, Nix, environmental settings)

--- home/shared/programs/gptcommit.nix
+++ home/shared/programs/gptcommit.nix
@@ -1,4 +1,5 @@
 {pkgs, ...}: {
-  home.file = {".config/gptcommit.env".source = ./dotfiles/gptcommit.env;};
+  home.file = {".config/gptcommit/gptcommit.env".source = ./dotfiles/gptcommit/gptcommit.env;};
+  home.file = {".config/gptcommit/config.toml".source = ./dotfiles/gptcommit/gptcommit.toml;};
   home.packages = with pkgs; [gptcommit];
 }
