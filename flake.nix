0068e441ab8f425df971d03adfca44061275e064
Markus Korn <markus.korn@gmail.com>
2024-10-11 20:09:24 +0200

    dont use unstable anymore, we use the 24.05 release
    recent alpha of tmux in unstable does not work for me

--- flake.nix
+++ flake.nix
@@ -4,10 +4,10 @@
   inputs = {
     # Where we get most of our software. Giant mono repo with recipes
     # called derivations that say how to build software.
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
 
     # Manages configs links things into your home directory
-    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # Controls system level software and settings including fonts
