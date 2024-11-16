be1b211e844046b7cfad0b14e1c430461c835878
Markus Korn <markus.korn@gmail.com>
2024-11-16 17:06:59 +0100

    Update Nix configurations for 24.11 release
    - Update Nix packages source to `release-24.11` for stability improvements.
    - Ensure project dependencies align with the latest stable NixOS release.

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     # Where we get most of our software. Giant mono repo with recipes
     # called derivations that say how to build software.
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
 
     # Manages configs links things into your home directory
     home-manager.url = "github:nix-community/home-manager/master";
