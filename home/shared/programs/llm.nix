51252770cbe819fbf11019e8d2d232ed7f6894f6
Markus Korn <markus.korn@gmail.com>
2025-01-31 11:00:34 +0100

    feat: add llm.env configuration file and update llm.nix
    Added a new environment configuration file for LLM (llm.env) that includes the OpenAI API key. Updated the llm.nix file to source this new environment file, removing references to gptcommit configurations.

--- home/shared/programs/llm.nix
+++ home/shared/programs/llm.nix
@@ -1,6 +1,5 @@
 {pkgs, ...}: {
-  home.file = {".config/gptcommit/gptcommit.env".source = ./dotfiles/gptcommit/gptcommit.env;};
-  home.file = {".config/gptcommit/config.toml".source = ./dotfiles/gptcommit/gptcommit.toml;};
+  home.file = {".config/llm/llm.env".source = ./dotfiles/llm/llm.env;};
   home.packages = with pkgs; [
     #(pkgs.callPackage ./vendor/gptcommit.thekorn.nix {inherit (pkgs.darwin.apple_sdk.frameworks) Security SystemConfiguration;})
     gptcommit
