3f3991e5e536295916b3e50f535e29cfe3eea5d8
Markus Korn <markus.korn@gmail.com>
2025-02-16 00:01:44 +0100

    Enhance update-nixos function with new commands
    - Enhance `update-nixos` function by adding support for updating Nix flakes and improving Neovim updates.
    - Ensure changes to the Nix flake lock file are committed automatically within the function.

--- home/shared/programs/dotfiles/zsh_custom/functions.zsh
+++ home/shared/programs/dotfiles/zsh_custom/functions.zsh
@@ -86,7 +86,10 @@ function update-nixos() (
   set -e
   cd ~/.config/nix
   git pull
+  nix flake update --commit-lock-file
+  git pu
   sudo nixos-rebuild switch --flake ~/.config/nix/.#
+  updateNvim
 )
 
 function updateBrew() (
