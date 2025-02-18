070a80d474441f5fc311d48a31cef8a98be42dc1
Markus Korn <markus.korn@gmail.com>
2025-02-18 05:10:53 +0100

    Update packages to use 'llm' instead of 'gptcommit'
    - Replace `gptcommit` with `llm` in the `home/packages` Nix configuration.
    - Comment out previous `gptcommit` package call.
    - Include updated `gptcommit` in home packages.

--- home/shared/programs/gptcommit.nix
+++ home/shared/programs/gptcommit.nix
@@ -2,6 +2,7 @@
   home.file = {".config/gptcommit/gptcommit.env".source = ./dotfiles/gptcommit/gptcommit.env;};
   home.file = {".config/gptcommit/config.toml".source = ./dotfiles/gptcommit/gptcommit.toml;};
   home.packages = with pkgs; [
-    llm
+    #(pkgs.callPackage ./vendor/gptcommit.thekorn.nix {inherit (pkgs.darwin.apple_sdk.frameworks) Security SystemConfiguration;})
+    gptcommit
   ];
 }
--- home/shared/programs/llm.nix
+++ home/shared/programs/llm.nix
@@ -1,7 +1,6 @@
 {pkgs, ...}: {
   home.file = {".config/llm/llm.env".source = ./dotfiles/llm/llm.env;};
   home.packages = with pkgs; [
-    #(pkgs.callPackage ./vendor/gptcommit.thekorn.nix {inherit (pkgs.darwin.apple_sdk.frameworks) Security SystemConfiguration;})
-    gptcommit
+    llm
   ];
 }
