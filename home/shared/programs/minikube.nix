0f8c64649c1c384551126787510841a5bde1e591
Markus Korn <markus.korn@gmail.com>
2025-06-20 12:04:29 +0200

    Add systemd service for Minikube autostart
    - Integrate a systemd service for automated Minikube management.
    - Ensure Minikube starts post-Docker service initialization.
    - Implement a persistent logging mechanism for service status.

--- home/shared/programs/minikube.nix
+++ home/shared/programs/minikube.nix
@@ -6,4 +6,33 @@
   programs.zsh.shellAliases = {
     "kubectl" = "minikube kubectl --";
   };
+
+  systemd.services.minikube = {
+    # https://joepreludian.medium.com/how-to-start-up-minikube-automatically-via-system-d-2cad99fd79bf
+    # this service is "wanted by" (see systemd man pages, or other tutorials) the system
+    # level that allows multiple users to login and interact with the machine non-graphically
+    # (see the Red Hat tutorial or Arch Linux Wiki for more information on what each target means)
+    # this is the "node" in the systemd dependency graph that will run the service
+    wantedBy = [ "multi-user.target" ];
+    # systemd service unit declarations involve specifying dependencies and order of execution
+    # of systemd nodes; here we are saying that we want our service to start after the network has
+    # set up (as our IRC client needs to relay over the network)
+    after = [ "docker.service" ];
+    description = "start minikube";
+    serviceConfig = {
+      # see systemd man pages for more information on the various options for "Type": "notify"
+      # specifies that this is a service that waits for notification from its predecessor (declared in
+      # `after=`) before starting
+      Type = "oneshot";
+      # username that systemd will look for; if it exists, it will start a service associated with that user
+      User = "thekorn";
+      group = "users";
+      # the command to execute when the service starts up
+      ExecStart = ''${pkgs.minikube}/bin/minikube start --driver=docker'';
+      # and the command to execute
+      ExecStop = ''${pkgs.minikube}/bin/minikube stop'';
+      RemainAfterExit = true;
+      StandardOutput = "journal"
+    };
+  };
 }
