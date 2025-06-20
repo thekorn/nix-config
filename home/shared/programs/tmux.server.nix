fb8ec8a1d9c1905b3b12428a254dad7f73c0acf2
Markus Korn <markus.korn@gmail.com>
2025-06-20 09:33:43 +0200

    Integrate tmux sessionizer for enhanced sessions
    - Implement a `sessionizer` script for efficient tmux session management, enabling creation, attachment, or switching of sessions.
    - Add new key bindings in tmux for launching and starting the sessionizer, enhancing user interaction.
    - Refine tmux configuration to support improved session management and terminal rendering.

--- home/shared/programs/tmux.server.nix
+++ home/shared/programs/tmux.server.nix
@@ -2,7 +2,42 @@
   pkgs,
   config,
   ...
-}: {
+}: let
+  sessionizer = pkgs.writeShellScriptBin "tmux-sessionizer" ''
+    ## from https://github.com/mrnugget/dotfiles/blob/master/bin/tmux-sessionizer
+
+    if [[ $# -eq 1 ]]; then
+        selected=$1
+    else
+        specific_directories=("/tmp" "~/.config/nix" "~/.config/nvim")
+        combined_directories=($(${pkgs.fd}/bin/fd -p -t d -d 3 --min-depth 3 . ~/devel/ | sed "s;$HOME;~;") "''${specific_directories[@]}")
+        selected=$(printf '%s\n' "''${combined_directories[@]}" | ${pkgs.fzf}/bin/fzf)
+    fi
+
+    if [[ -z $selected ]]; then
+        exit 0
+    fi
+
+    selected=$(echo $selected | sed "s;~;$HOME;")
+
+    selected_name=$(basename "$selected" | tr . _)
+    tmux_running=$(pgrep tmux)
+    if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
+        ${pkgs.tmux}/bin/tmux new-session -s $selected_name -c $selected
+        exit 0
+    fi
+
+    if ! ${pkgs.tmux}/bin/tmux has-session -t $selected_name 2>/dev/null; then
+        ${pkgs.tmux}/bin/tmux new-session -ds $selected_name -c $selected
+    fi
+
+    if [[ -z $TMUX ]]; then
+        ${pkgs.tmux}/bin/tmux attach-session -t $selected_name
+    else
+        ${pkgs.tmux}/bin/tmux switch-client -t $selected_name
+    fi
+  '';
+in {
   programs.tmux = {
     enable = true;
     historyLimit = 5000;
@@ -32,6 +67,9 @@
 
       set-option -g detach-on-destroy off       # dont quit the terminal session if there is at least one other tmux session running
       set -g status-left-length 32              # we have more space on the session name field
+
+      bind-key S run-shell "tmux popup -E ${sessionizer}/bin/tmux-sessionizer"
+      bind-key N run-shell "${sessionizer}/bin/tmux-sessionizer ~/.config/nix"
     '';
   };
 }
