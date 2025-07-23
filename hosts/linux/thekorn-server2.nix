a83294917e7f1b10c3c5d97ff21ea4ad547a7829
Markus Korn <markus.korn@gmail.com>
2025-07-23 20:41:07 +0200

    Configure Linux server with updated settings
    - Update and configure thekorn-server2 with new hardware settings and German locale.
    - Enable essential system packages and shell environment.
    - Optimize server settings by adjusting SSH, firewall, and Nix features.

--- hosts/linux/thekorn-server2.nix
+++ hosts/linux/thekorn-server2.nix
@@ -8,7 +8,7 @@
 }: {
   imports = [
     # Include the results of the hardware scan.
-    ./configurations/thekorn-server/hardware-configuration.nix
+    ./configurations/thekorn-server2/hardware-configuration.nix
 
     ./shared/home.private.nix
     #./shared/virtualisation.nix
@@ -16,25 +16,17 @@
     ../shared/certificates.nix
   ];
 
-  # Bootloader.s
+  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
+  networking.hostName = "thekorn-server";
 
   # Set your time zone.
   time.timeZone = "Europe/Berlin";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = "de_DE.UTF-8";
 
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "de_DE.UTF-8";
@@ -48,54 +40,24 @@
     LC_TIME = "de_DE.UTF-8";
   };
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.thekorn = {
-    isNormalUser = true;
-    description = "thekorn";
-    extraGroups = ["networkmanager" "wheel"];
-    packages = with pkgs; [];
-  };
-
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
-  ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
+  programs.zsh.enable = true;
+  programs.gnupg.agent.enable = true;
+  environment = {
+    shells = with pkgs; [bash zsh];
+    systemPackages = [pkgs.coreutils];
+  };
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
+  networking.firewall.enable = false;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  system.stateVersion = "25.05";
+  nix.settings.experimental-features = ["nix-command" "flakes"];
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
+  programs.nix-ld.enable = true;
+
+  services.tailscale.enable = false;
 }
