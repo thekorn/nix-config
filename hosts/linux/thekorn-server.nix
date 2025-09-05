04e5ccd774eaafca1e24e39c049feb353b2e4d31
Markus Korn <markus.korn@gmail.com>
2025-09-05 20:36:33 +0200

    linux boot: set max of entries to 10

--- hosts/linux/thekorn-server.nix
+++ hosts/linux/thekorn-server.nix
@@ -16,6 +16,7 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.configurationLimit = 10;
 
   networking.hostName = "thekorn-server";
 
