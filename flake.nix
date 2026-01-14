313072e80cd33fabd3c2fd6c25f2a8ad2e42ea9c
Markus Korn <markus.korn@gmail.com>
2026-01-14 08:09:50 +0100

    Update dependencies to unstable Nixpkgs branch
    - Revert `nixpkgs` source to `nixpkgs-unstable` branch for more recent updates.

--- flake.nix
+++ flake.nix
@@ -4,8 +4,8 @@
   inputs = {
     # Where we get most of our software. Giant mono repo with recipes
     # called derivations that say how to build software.
-    # nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nixpkgs.url = "github:nixos/nixpkgs/5912c17"; # https://github.com/NixOS/nixpkgs/issues/478546
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    #nixpkgs.url = "github:nixos/nixpkgs/5912c17"; # https://github.com/NixOS/nixpkgs/issues/478546
 
     # Manages configs links things into your home directory
     home-manager.url = "github:nix-community/home-manager/master";
