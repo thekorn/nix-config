e13f55a8a1c96348de814c3a65c41e536ba62da9
Markus Korn <markus.korn@gmail.com>
2026-01-25 15:49:47 +0100

    Refactor imports in Nix configuration files
    - Clean up and streamline server configuration by removing unnecessary imports.
    - Re-enable shared imports for consistent application of common configurations across the server setup.

--- home/shared/common.programs.server.nix
+++ home/shared/common.programs.server.nix
@@ -5,13 +5,10 @@
     ./programs/fzf.nix
     ./programs/git.server.nix
     ./programs/tmux.server.nix
-    ./programs/zsh.server.nix
     ./devel.nix
     ./programs/nvim.nix
     ./programs/lazygit.nix
     ./programs/ripgrep.nix
     ./programs/zig.nix
-    ./programs/oh-my-posh.server.nix
-    #./programs/minikube.nix
   ];
 }
