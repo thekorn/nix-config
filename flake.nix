696054da882e160e0d8d32b63baf7cc6b1466590
Markus Korn <markus.korn@gmail.com>
2026-01-28 03:24:54 +0100

    Clarify Swift version issue in documentation
    - Add explanatory comment in `flake.nix` regarding Swift version issue in nixpkgs.

--- flake.nix
+++ flake.nix
@@ -5,7 +5,9 @@
     # Where we get most of our software. Giant mono repo with recipes
     # called derivations that say how to build software.
     #nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    nixpkgs.url = "github:nixos/nixpkgs/ed142ab"; #swift 5.10.1 is broken on newer builds
+    # swift 5.10.1 is broken on newer builds
+    # https://github.com/NixOS/nixpkgs/issues/483584
+    nixpkgs.url = "github:nixos/nixpkgs/ed142ab";
 
     # Manages configs links things into your home directory
     home-manager.url = "github:nix-community/home-manager/master";
