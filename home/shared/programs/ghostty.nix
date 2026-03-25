5bc602e21002d6ba16788ed722aa5621617990a0
Markus Korn <markus.korn@gmail.com>
2026-03-25 06:32:38 +0100

    Refactor ghostty configuration with customization options
    - Refactor ghostty setup for modular configuration and Zsh integration.
    - Enhance customization options for ghostty with themes, fonts, and behavior settings.

--- home/shared/programs/ghostty.nix
+++ home/shared/programs/ghostty.nix
@@ -1,20 +1,20 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [ghostty-bin];
-  home.file = {".config/ghostty/config".source = ./dotfiles/ghostty/config;};
-  programs.zsh = {
-    localVariables = {
-      GHOSTTY_RESOURCES_DIR = "${pkgs.ghostty-bin}/Applications/Ghostty.app/Contents/Resources/ghostty";
-    };
-    initContent = ''
-      if [[ -n $GHOSTTY_RESOURCES_DIR ]]; then
-        autoload -Uz -- "$GHOSTTY_RESOURCES_DIR"/shell-integration/zsh/ghostty-integration
-        #ghostty-integration
-        #unfunction ghostty-integration
-      fi
-    '';
-
-    shellAliases = {
-      ghostty = "${pkgs.ghostty-bin}/Applications/Ghostty.app/Contents/MacOS/ghostty";
+  programs.ghostty = {
+    enable = true;
+    package = pkgs.ghostty-bin;
+    enableZshIntegration = true;
+    settings = {
+      theme = "nord";
+      font-family = "GeistMono Nerd Font";
+      font-size = 12;
+      font-thicken = true;
+      confirm-close-surface = false;
+      auto-update = "off";
+      copy-on-select = "clipboard";
+      quit-after-last-window-closed = true;
+      cursor-style-blink = true;
+      shell-integration-features = "no-cursor, sudo, title";
+      config-file = "?custom";
     };
   };
 }
