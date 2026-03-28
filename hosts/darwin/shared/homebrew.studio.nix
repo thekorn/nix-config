d1c8fc3143d8e2f849c770130421cf3c9c2b2446
Markus Korn <markus.korn@gmail.com>
2026-03-28 16:07:32 +0100

    Update program configurations and dependencies
    - Add "nvidia-geforce-now" to the list of casks in homebrew.studio.nix
    - Update zed.nix to include the "oxc" extension in the zed-editor config
    - Change the user settings and base keymap in zed.nix to match VSCode configurations

--- hosts/darwin/shared/homebrew.studio.nix
+++ hosts/darwin/shared/homebrew.studio.nix
@@ -3,14 +3,13 @@
     enable = true;
     caskArgs.no_quarantine = true;
     global.brewfile = true;
-    masApps = {};
+    masApps = { };
     casks = [
       "obs"
       "steam"
       "parallels"
       "multipass"
-      # "utm"
-      # "nvidia-geforce-now"
+      "nvidia-geforce-now"
     ];
     brews = [
     ];
