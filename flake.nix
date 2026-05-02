b668c3eba8c63173d3b280ead9a457fd4211b2eb
Markus Korn <markus.korn@gmail.com>
2026-05-02 19:40:44 +0200

    chore: Update NixOS configurations and nixpkgs URL
    - Update `nixpkgs` URL to `nixpkgs-unstable` branch
    - Remove commented out `nixpkgs` URLs
    - Add and configure `nixosConfigurations` for `thekorn-server` and `thekorn-server2`

--- flake.nix
+++ flake.nix
@@ -4,9 +4,9 @@
   inputs = {
     # Where we get most of our software. Giant mono repo with recipes
     # called derivations that say how to build software.
-    #nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     #nixpkgs.url = "github:nixos/nixpkgs/2175e5c";
-    nixpkgs.url = "github:nixos/nixpkgs/master";
+    #nixpkgs.url = "github:nixos/nixpkgs/master";
 
     # Manages configs links things into your home directory
     home-manager.url = "github:nix-community/home-manager/master";
