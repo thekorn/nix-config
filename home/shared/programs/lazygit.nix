04e4057f0da62dbe135ab652807ddf79d1c6e5d2
Markus Korn <markus.korn@gmail.com>
2026-06-23 16:46:35 +0200

    refactor: Refactor program configurations and custom functions
    - Enable `lazygit` with specific settings for the GUI
    - Add custom file types for ripgrep
    - Remove unused functions `pretty_git_log` and `remove_untracked_files` from `githelpers.sh`
    - Delete unnecessary files: `functions.zsh`, `.ripgreprc`, `functions.server.zsh`, and `lazygit.yml`

--- home/shared/programs/lazygit.nix
+++ home/shared/programs/lazygit.nix
@@ -1,7 +1,12 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [lazygit];
-  home.file = {".config/lazygit/config.yml".source = ./dotfiles/lazygit.yml;};
-  home.sessionVariables = {
-    LG_CONFIG_FILE = "$HOME/.config/lazygit/config.yml";
+  programs.lazygit = {
+    enable = true;
+    settings = {
+      gui = {
+        showIcons = true;
+        showRandomTip = false;
+        notARepository = "quit";
+      };
+    };
   };
 }
--- home/shared/programs/ripgrep.nix
+++ home/shared/programs/ripgrep.nix
@@ -1,4 +1,20 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [ripgrep];
-  home.file = {".ripgreprc".source = ./dotfiles/.ripgreprc;};
+  programs.ripgrep = {
+    enable = true;
+    arguments = [
+      "--max-columns=150"
+      "--max-columns-preview"
+
+      # Add my 'web' type.
+      "--type-add"
+      "web:*.{html,css,js}*"
+
+      # Add my 'ts' type.
+      "--type-add"
+      "ts:*.{ts,tsx,vue}"
+
+      # Because who cares about case!?
+      "--smart-case"
+    ];
+  };
 }
