1184f11cd8d13d2434b12872543c5db83861238e
Markus Korn <markus.korn@gmail.com>
2026-08-01 19:33:25 +0200

    chore: Revise package configuration for macOS in Ladybird Nix script
    - Update package configuration to only install Homebrew packages on macOS
    - Refactor package list in ladybird.nix to improve readability

--- home/shared/devel/ladybird.nix
+++ home/shared/devel/ladybird.nix
@@ -3,35 +3,33 @@
   username,
   ...
 }: {
-  homebrew.brews = [
-    "autoconf"
-    "autoconf-archive"
-    "automake"
-    "ccache"
-    "cmake"
-    "libtool"
-    "nasm"
-    "ninja"
-    "pkg-config"
-    "llvm@20"
-  ];
+  # in case the nix packages dont work on darwin...
+  #homebrew.brews = pkgs.lib.optionals (pkgs.stdenv.isDarwin) [
+  #  "autoconf"
+  #  "autoconf-archive"
+  #  "automake"
+  #  "ccache"
+  #  "cmake"
+  #  "libtool"
+  #  "nasm"
+  #  "ninja"
+  #  "pkg-config"
+  #  "llvm@20"
+  #];
 
-  home-manager.users.${username}.home.packages = with pkgs;
-    [
-      cargo
-      rustc
-    ]
-    ++ lib.optionals (!stdenv.isDarwin) [
-      autoconf
-      autoconf-archive
-      automake
-      ccache
-      cmake
-      ffmpeg
-      libtool
-      llvm
-      nasm
-      ninja
-      pkg-config
-    ];
+  home-manager.users.${username}.home.packages = with pkgs; [
+    cargo
+    rustc
+    autoconf
+    autoconf-archive
+    automake
+    ccache
+    cmake
+    ffmpeg
+    libtool
+    llvm
+    nasm
+    ninja
+    pkg-config
+  ];
 }
